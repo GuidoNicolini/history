4b4bb0778878eeda9d0672534ec808687be64ab9
GuidoNicolini <guidonicolini123@hotmail.com>
2026-04-09 02:58:34 -0300

    electro working

---
+++ framework/main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Carga el archivo index.html generado por Angular
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/framework/browser/index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+
+  // Abre las herramientas de desarrollo (opcional)
+  // win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
---
+++ framework/src/app/shared/views/start-view/start-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-view',
+  standalone: false,
+  templateUrl: './start-view.html',
+  styleUrl: './start-view.css',
+})
+export class StartView {
+
+}
