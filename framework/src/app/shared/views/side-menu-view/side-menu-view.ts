f8f49bae6394a51e0f3b26a124f46e922574fc35
GuidoNicolini <guidonicolini123@hotmail.com>
2026-05-03 17:15:08 -0300

    sumar

---
+++ framework/src/app/shared/views/side-menu-view/side-menu-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu-view',
+  standalone: false,
+  templateUrl: './side-menu-view.html',
+  styleUrl: './side-menu-view.css',
+})
+export class SideMenuVIew {
+
+}
